879ec3e3709a430608f1d3d19ec2adbd05b38ed3
isaacbur81345 <106206148+isaacbur81345@users.noreply.github.com>
2024-11-10 15:29:50 -0700

    Create ok.lua

---
+++ ok.lua
@@ -0,0 +1,70 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local g = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+local ImageLabel_2 = Instance.new("ImageLabel")
+local ImageLabel_3 = Instance.new("ImageLabel")
+local ImageLabel_4 = Instance.new("ImageLabel")
+local ImageLabel_5 = Instance.new("ImageLabel")
+local ImageLabel_6 = Instance.new("ImageLabel")
+
+--Properties:
+
+g.Name = "g"
+g.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+g.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageLabel.Parent = g
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel.Image = "rbxassetid://75857255600984"
+
+ImageLabel_2.Parent = g
+ImageLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_2.BackgroundTransparency = 1.000
+ImageLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_2.BorderSizePixel = 0
+ImageLabel_2.Position = UDim2.new(-0.204334363, 0, 0.215588719, 0)
+ImageLabel_2.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel_2.Image = "rbxassetid://75857255600984"
+
+ImageLabel_3.Parent = g
+ImageLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_3.BackgroundTransparency = 1.000
+ImageLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_3.BorderSizePixel = 0
+ImageLabel_3.Position = UDim2.new(0.280185759, 0, 0.422885567, 0)
+ImageLabel_3.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel_3.Image = "rbxassetid://75857255600984"
+
+ImageLabel_4.Parent = g
+ImageLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_4.BackgroundTransparency = 1.000
+ImageLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_4.BorderSizePixel = 0
+ImageLabel_4.Position = UDim2.new(-0.32198143, 0, -0.427860707, 0)
+ImageLabel_4.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel_4.Image = "rbxassetid://75857255600984"
+
+ImageLabel_5.Parent = g
+ImageLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_5.BackgroundTransparency = 1.000
+ImageLabel_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_5.BorderSizePixel = 0
+ImageLabel_5.Position = UDim2.new(0.395510823, 0, -0.208955228, 0)
+ImageLabel_5.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel_5.Image = "rbxassetid://75857255600984"
+
+ImageLabel_6.Parent = g
+ImageLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_6.BackgroundTransparency = 1.000
+ImageLabel_6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_6.BorderSizePixel = 0
+ImageLabel_6.Position = UDim2.new(-0.0402476788, 0, -0.412935317, 0)
+ImageLabel_6.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel_6.Image = "rbxassetid://75857255600984"
